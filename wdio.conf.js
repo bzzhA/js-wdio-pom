3f65d0ee641f6e0789fcc799a6c07e105e28d8a0
Artem Sidorovich <unstpbdl@gmail.com>
2023-02-02 22:04:14 +0300

    fixed: "Google Cloud Calculator"

--- wdio.conf.js
+++ wdio.conf.js
@@ -62,25 +62,34 @@ export const config = {
     // Sauce Labs platform configurator - a great tool to configure your capabilities:
     // https://saucelabs.com/platform/platform-configurator
     //
-    capabilities: [{
-    
-        // maxInstances can get overwritten per capability. So if you have an in-house Selenium
-        // grid with only 5 firefox instances available you can make sure that not more than
-        // 5 instances get started at a time.
-        maxInstances: 5,
-        //
-        // browserName: 'chrome',
-        browserName: 'firefox',
-        acceptInsecureCerts: true
-        // If outputDir is provided WebdriverIO can capture driver session logs
-        // it is possible to configure which logTypes to include/exclude.
-        // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
-        // excludeDriverLogs: ['bugreport', 'server'],
-    }],
+    capabilities: [
     //     {
-    //         maxInstances: 5,
-    //         browserName: 'safari',
-    // }],
+    //
+    //     // maxInstances can get overwritten per capability. So if you have an in-house Selenium
+    //     // grid with only 5 firefox instances available you can make sure that not more than
+    //     // 5 instances get started at a time.
+    //     maxInstances: 1,
+    //     browserName: 'chrome',
+    //     acceptInsecureCerts: true
+    //     'wdio:devtoolsOptions':{
+    //         headless: true
+    //     },
+    //     "goog:chromeOptions": {
+    //         args: ['--headless', '--no-sandbox']
+    //     },
+    //     // If outputDir is provided WebdriverIO can capture driver session logs
+    //     // it is possible to configure which logTypes to include/exclude.
+    //     // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
+    //     // excludeDriverLogs: ['bugreport', 'server'],
+    // },
+        {
+        maxInstances: 1,
+        browserName: 'firefox',
+        acceptInsecureCerts: true,
+        "moz:firefoxOptions": {
+            args: ['--headless']
+        },
+    }],
     //
     // ===================
     // Test Configurations
@@ -112,7 +121,7 @@ export const config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://localhost',
+    baseUrl: 'http://localhost:8080',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
@@ -129,6 +138,7 @@ export const config = {
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
     services: ['selenium-standalone'],
+    // services: ['chromedriver'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
